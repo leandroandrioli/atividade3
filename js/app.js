ba0674544d1ef9b460327b710704ffcaf5365cc1
leandroandrioli <leandro.pga@gmail.com>
2025-10-23 11:26:17 -0300

    versao final entrega3

--- js/app.js
+++ js/app.js
@@ -6,3 +6,20 @@ const menu = document.getElementById('menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+
+    if (className === 'dark-theme') {
+        this.textContent = 'Light';
+    }else {
+        this.textContent = 'Dark';
+    }
+
+    console.log('current class name: ' + className);
+});
+  
